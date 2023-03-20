e4c01275e063da3e640120fed2ac7f6615a0a110
tomwilsonsco <thomaswilson81@gmail.com>
2023-03-20 10:47:29 +0000

    remove id and styling

--- R/extract_pixels.R
+++ R/extract_pixels.R
@@ -4,16 +4,16 @@ extract_per_class <- function(class_value,
                               training_shapes,
                               training_class_col) {
   class_training <- training_shapes[training_shapes[[training_class_col]]
-                                    ==class_value, ]
+  == class_value, ]
 
-  pixel_vals <- terra::extract(input_raster, class_training)
+  pixel_vals <- terra::extract(input_raster, class_training, ID = FALSE)
 
   pixel_vals[[training_class_col]] <- class_value
   pixel_vals
 }
 
-check_class_col <- function(training_shapes, class_column){
-  if (! class_column %in% colnames(training_shapes)){
+check_class_col <- function(training_shapes, class_column) {
+  if (!class_column %in% colnames(training_shapes)) {
     training_shapes[[class_column]] <- 0
   }
   training_shapes
@@ -46,24 +46,27 @@ get_distinct_classes <- function(training_shapes, training_class_col) {
 #' @examples
 #' # create raster
 #' rast <- terra::rast(
-#' ncols = 10, nrows = 10, xmin = 0, xmax = 10, ymin = 0,
-#' ymax = 10, crs = "EPSG:4326", vals=runif(100, 0, 1),
-#' names=c("b1")
+#'   ncols = 10, nrows = 10, xmin = 0, xmax = 10, ymin = 0,
+#'   ymax = 10, crs = "EPSG:4326", vals = runif(100, 0, 1),
+#'   names = c("b1")
 #' )
 #'
 #' # create polygon vector layer
-#'p <- list(sf::st_polygon(list(cbind(c(0,1,1,0,0), c(0,0,1,1,0)))),
-#'           sf::st_polygon(list(cbind(c(8, 9, 9, 8, 8), c(8, 8, 9, 9, 8)))))
+#' p <- list(
+#'   sf::st_polygon(list(cbind(c(0, 1, 1, 0, 0), c(0, 0, 1, 1, 0)))),
+#'   sf::st_polygon(list(cbind(c(8, 9, 9, 8, 8), c(8, 8, 9, 9, 8))))
+#' )
 #'
-#' vect <- sf::st_as_sf(data.frame(id=c(1,2),
-#' geom=sf::st_sfc(p, crs="EPSG:4326")))
+#' vect <- sf::st_as_sf(data.frame(
+#'   id = c(1, 2),
+#'   geom = sf::st_sfc(p, crs = "EPSG:4326")
+#' ))
 #'
 #' # extract raster pixels where intersect vector later
 #' extract_pixels(rast, vect)
 extract_pixels <- function(input_raster,
                            training_shapes,
                            training_class_col = "ml_class") {
-
   is_raster(input_raster)
 
   is_vector(training_shapes)
@@ -80,5 +83,4 @@ extract_pixels <- function(input_raster,
     training_shapes = training_shapes,
     training_class_col = training_class_col
   )
-
 }
