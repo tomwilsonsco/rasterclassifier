864d49f4dede0ba1bf5c5df6bc45462dee44f9b8
tomwilsonsco <thomaswilson81@gmail.com>
2023-03-20 10:48:07 +0000

    remove id and attrib test

--- tests/testthat/test-extract_pixels.R
+++ tests/testthat/test-extract_pixels.R
@@ -1,6 +1,5 @@
-test_that("polygon extract works", {
+test_that("polygon extract add class attribute works", {
   expect_df <- data.frame(
-    ID = c(1, 2),
     b1 = c(0.1306957, 0.3279207),
     ml_class = c(0, 0)
   )
@@ -24,9 +23,8 @@ test_that("polygon extract works", {
   expect_equal(extract_pixels(rast, vect), expect_df, tolerance = 0.001)
 })
 
-test_that("point extract works", {
+test_that("point extract add class attribute works", {
   expect_df <- data.frame(
-    ID = c(1, 2, 3, 4),
     b1 = c(0.4089769, 0.8924190, 0.4753166, 0.3517979),
     ml_class = c(0, 0)
   )
@@ -47,4 +45,32 @@ test_that("point extract works", {
 
 
   expect_equal(extract_pixels(rast, pt_vect), expect_df, tolerance = 0.001)
+})
+
+
+test_that("point extract existing class col works", {
+  expect_df <- data.frame(
+    b1 = c(0.4089769, 0.8924190, 0.4753166, 0.3517979),
+    train_class = c(0, 0, 1, 1)
+  )
+
+  set.seed(123)
+  rast <- terra::rast(
+    ncols = 10, nrows = 10, xmin = 0, xmax = 10, ymin = 0,
+    ymax = 10, crs = "EPSG:4326",
+    names = c("b1"),
+    vals = runif(100, 0, 1)
+  )
+
+  set.seed(123)
+  df <- data.frame(x = runif(4, 0, 10), train_class = c(0, 0, 1, 1))
+  set.seed(321)
+  df$y <- runif(4, 0, 10)
+  pt_vect <- sf::st_as_sf(df, coords = c("x", "y"), crs = "EPSG:4326")
+
+
+  expect_equal(extract_pixels(rast, pt_vect, "train_class"),
+    expect_df,
+    tolerance = 0.001
+  )
 })
