ab128951c96d412936c8042aa556ddeb1e12cb98
tomwilsonsco <thomaswilson81@gmail.com>
2023-03-30 08:45:36 +0100

    y as factor vector

--- R/xy_training_split.R
+++ R/xy_training_split.R
@@ -1,24 +1,36 @@
-xy_select <- function(training_df, class_column) {
-  x <- training_df %>% dplyr::select(- dplyr::all_of(c(class_column)))
+xy_select <- function(training_df, class_column, y_as_factor) {
+  x <- training_df %>% dplyr::select(-dplyr::all_of(c(class_column)))
 
-  y <- training_df %>% dplyr::select(dplyr::all_of(c(class_column)))
+  y <- training_df %>%
+    dplyr::select(dplyr::all_of(c(class_column))) %>%
+    dplyr::pull()
+
+  if (y_as_factor) {
+    y <- as.factor(y)
+  }
 
   list(x = x, y = y)
 }
 
 
-#' Divide train and test dataframes to x and y columns.
+#' Divide train and test dataframes to x columns and y column.
 #'
 #' The list of train and test dataframes is split into sub lists of x and y.
 #'
 #' @param train_test_list List of dataframes.
 #' @param class_column Name of the column indicating y values - this column is
 #' dropped from x and is the only output of y.
+#' @param y_as_factor Should the y variable by converted to a factor datatype.
+#' Defaults to TRUE as required for many R ml classification algorithms.
 #'
 #' @return A list of  a list of x and y dataframes, for both train and test.
 #' @export
 #'
 #' @examples
-xy_selection <- function(train_test_list, class_column){
-  purrr::map(train_test_list, xy_split, class_column=class_column)
+xy_selection <- function(train_test_list, class_column, y_as_factor = TRUE) {
+  purrr::map(train_test_list,
+    xy_select,
+    class_column = class_column,
+    y_as_factor = y_as_factor
+  )
 }
