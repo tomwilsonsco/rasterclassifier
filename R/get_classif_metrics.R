0702c0f9b923690096262b7c152d5cd2d688c0e9
tomwilsonsco <thomaswilson81@gmail.com>
2023-04-05 09:53:01 +0100

    metrics to internal fns

--- R/get_classif_metrics.R
+++ R/get_classif_metrics.R
@@ -1,54 +1,78 @@
-calc_metrics <- function(pred_y, actual_y, micro = TRUE) {
+.calc_f1_score <- function(precision, recall) {
+  2 * (precision * recall) / (precision + recall)
+}
+
+# Binary classification situation
+.binary_metrics <- function(confusion_matrix) {
+  tp <- confusion_matrix[2, 2]
+  tn <- confusion_matrix[1, 1]
+  fp <- confusion_matrix[2, 1]
+  fn <- confusion_matrix[1, 2]
+
+  precision <- tp / sum(tp, fp)
+  recall <- tp / sum(tp, fn)
+
+  list(
+    confusion_matrix = confusion_matrix,
+    precision = precision,
+    recall = recall,
+    f1_score = .calc_f1_score(precision, recall),
+    overall = sum(tp, tn) / sum(tp, tn, fp, fn)
+  )
+}
+
+# Multiclass situation - macro precision and recall calc
+.multiclass_metrics <- function(confusion_matrix) {
+  tp <- diag(confusion_matrix)
+
+  row_sum <- rowSums(confusion_matrix)
+  col_sum <- colSums(confusion_matrix)
+
+  precision <- mean(tp / row_sum)
+  recall <- mean(tp / col_sum)
+
+  list(
+    confusion_matrix = confusion_matrix,
+    precision = precision,
+    recall = recall,
+    f1_score = .calc_f1_score(precision, recall),
+    overall = sum(tp) / sum(row_sum)
+  )
+}
+
+.get_metrics <- function(pred_y, actual_y) {
   # Ensure a square matrix
   actual_y <- factor(actual_y)
   pred_y <- factor(pred_y, levels = levels(actual_y))
 
   confusion_matrix <- table(pred_y, actual_y)
 
-  # Binary classification situation
-  if (ncol(confusion_matrix) == 2) {
-    tp <- confusion_matrix[2, 2]
-    tn <- confusion_matrix[1, 1]
-    fp <- confusion_matrix[2, 1]
-    fn <- confusion_matrix[1, 2]
-
-    precision <- tp / sum(tp, fp)
-    recall <- tp / sum(tp, fn)
-
-    overall <- sum(tp, tn) / sum(tp, tn, fp, fn)
-
-    # Multiclass classification situation
+  if (ncol(confusion_matrix == 2)) {
+    metrics <- .binary_metrics(confusion_matrix)
   } else if (ncol(confusion_matrix > 2)) {
-    tp <- diag(confusion_matrix)
-
-    row_sum <- rowSums(confusion_matrix)
-    col_sum <- colSums(confusion_matrix)
-
-    precision <- mean(tp / row_sum)
-    recall <- mean(tp / col_sum)
-
-    overall <- sum(tp) / sum(row_sum)
-
+    metrics <- .multiclass_metrics(confusion_matrix)
     # If just one class in actual_y
   } else {
     cli::abort("There is only one class: {levels(actual_y)}")
   }
-
-  f1_score <- 2 * (precision * recall) / (precision + recall)
-  message("Confusion matrix")
-
-  print(confusion_matrix)
-
-  # Output a list of metrics
-  list(
-    confusion_matrix = confusion_matrix,
-    precision = precision,
-    recall = recall,
-    f1_score = f1_score,
-    overall = overall
-  )
+  metrics
 }
 
-get_ml_metrics <- function(pred_y, actual_y, micro = TRUE) {
-  calc_metrics(pred_y, actual_y, micro)
+#' Calculate common classification metrics from prediction and actual vectors
+#'
+#' @param pred_y A vector of prediction class values. Should be factor
+#' or integer.
+#' @param actual_y A vector of actual class values. Should be the same datatype
+#' and of the same length as `pred_y`.
+#'.
+#' @return A list of metrics - confusion matrix, precision, recall, F1 Score
+#' and overall accuracy.
+#' @export
+#'
+#' @examples
+#' actual_y <- iris$Species
+#' pred_y <- sample(actual_y, length(actual_y), replace=TRUE)
+#' get_ml_metrics(actual_y, pred_y)
+get_ml_metrics <- function(pred_y, actual_y) {
+  .get_metrics(pred_y, actual_y)
 }
